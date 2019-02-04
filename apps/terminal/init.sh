1818433f660f12261c6a0635cd528f27cd019b10
Jms Dnns <jdennis@gmail.com>
2019-02-04 14:26:15 -0500

    signifiantly simplified terminal

--- apps/terminal/init.sh
+++ apps/terminal/init.sh
@@ -1,43 +1,9 @@
-## ArcOS Terminal
-## --------------
-## by Jms Dnns
-
-
-## Lofi Getopts
-
-NEW_PROFILE=NO
-APPLY_COLORS=NO
-
-for i in "$@"; do
-case $i in
-    -n|--new)
-    NEW_PROFILE=YES
-    shift # past argument=value
-    ;;
-    -c|--colors)
-    APPLY_COLORS=YES
-    shift # past argument=value
-    ;;
-    *)
-    # unknown option
-    ;;
-esac
-done
-
-
-## GSettings
+## New Profile
 
 SCHEMA_KEY="org.gnome.Terminal.Legacy.Profile"
 RELOC_KEY="/org/gnome/terminal/legacy/profiles"
-DEFAULT_PROFILE=$(gsettings get org.gnome.Terminal.ProfilesList default)
-PROFILE_KEY="$SCHEMA_KEY:$RELOC_KEY:/:$DEFAULT_PROFILE/"
-gset="gsettings set $PROFILE_KEY"
-
-
-## Profile
-
-DEFAULT_PROFILE=$(gsettings get org.gnome.Terminal.ProfilesList default)
-PROFILE_KEY="$SCHEMA_KEY:$RELOC_KEY:/:$DEFAULT_PROFILE/"
+PROFILE=$(uuidgen)
+PROFILE_KEY="$SCHEMA_KEY:$RELOC_KEY:/:$PROFILE/"
 gset="gsettings set $PROFILE_KEY"
 
 
@@ -56,12 +22,7 @@ $gset allow-bold false
 
 ## Colors
 
-if [[ "$APPLY_COLORS" = "YES" ]]; then
-    echo "WITH COLORS"
-    $gset use-theme-colors "false"
-    $gset background-color "'#262B36'"
-    $gset foreground-color "'#D8DEE9'"
-    $gset palette "['#3B4252', '#BF616A', '#A3BE8C', '#EBCB8B', '#81A1C1', '#B48EAD', '#88C0D0', '#E5E9F0', '#4C566A', '#BF616A', '#A3BE8C', '#EBCB8B', '#81A1C1', '#B48EAD', '#8FBCBB', '#ECEFF4']"
-else
-    echo "NO COLORS"
-fi
+$gset use-theme-colors "false"
+$gset background-color "'#262B36'"
+$gset foreground-color "'#D8DEE9'"
+$gset palette "['#3B4252', '#BF616A', '#A3BE8C', '#EBCB8B', '#81A1C1', '#B48EAD', '#88C0D0', '#E5E9F0', '#4C566A', '#BF616A', '#A3BE8C', '#EBCB8B', '#81A1C1', '#B48EAD', '#8FBCBB', '#ECEFF4']"
